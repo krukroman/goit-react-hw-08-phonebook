94f33f4d0f5a616459d5b29889957378d5fef074
Roman Kruk <wilmlass@gmail.com>
2021-11-11 23:47:09 +0200

    corrections

--- src/components/Copyright/Copiright.js
+++ src/components/Copyright/Copiright.js
@@ -1,3 +1,4 @@
+import { Link as RouterLink } from 'react-router-dom';
 import { Box, Typography, Link } from '@mui/material';
 
 export default function Copyright(props = false) {
@@ -5,7 +6,7 @@ export default function Copyright(props = false) {
     <Box component="footer" {...props}>
       <Typography variant="body2" color="text.secondary" align="center">
         {'Copyright © '}
-        <Link color="inherit" href="/">
+        <Link color="inherit" component={RouterLink} to="/">
           Phonebook
         </Link>{' '}
         {new Date().getFullYear()}
